2145bff2af395931578ff0beaa56f2b2aa914a2a
Andrei Vasileuski <vasileuski.andrei@gmail.com>
2022-02-17 23:31:16 +0300

    refactoring validateData method in PlaceBetValidator class

--- src/main/java/com/company/validator/PlaceBetValidator.java
+++ src/main/java/com/company/validator/PlaceBetValidator.java
@@ -1,7 +1,7 @@
 package com.company.validator;
 
-import com.company.dto.BetDto;
 import com.company.dto.PlaceBetDto;
+import com.company.exception.ValidationException;
 import lombok.NoArgsConstructor;
 
 import java.util.*;
@@ -14,48 +14,63 @@ public class PlaceBetValidator implements Validator<PlaceBetDto> {
     private static final PlaceBetValidator INSTANCE = new PlaceBetValidator();
     private static final Map<Integer, List<Integer>> lotIdsAndBiddersIds = new HashMap<>();
 
+    private PlaceBetDto placeBetDto;
+    private ValidationResult validationResult;
+
 
     @Override
     public ValidationResult validateData(PlaceBetDto object) {
-        var validationResult = new ValidationResult();
-        var lotId = object.getLotId();
-        var strUserBet = object.getUserBet();
-        Integer userBet = null;
+        placeBetDto = object;
+        validationResult = new ValidationResult();
 
-        if (!strUserBet.matches("[1-9]+")) {
+        checkInputCharacterInBetField();
+        checkDoubleBetInARow();
+
+        return validationResult;
+    }
+
+    private void checkDoubleBetInARow() {
+        var lotId = placeBetDto.getLotId();
+        var userId = placeBetDto.getUserId();
+
+        if (placeBetDto.getUserName().equals(placeBetDto.getUserPlacedLastBet())) {
             validationResult.add(Error.of("double-bet", "You can't place two bets in a row"));
-            return validationResult;
-        } else {
-            userBet = Integer.valueOf(strUserBet);
-        }
-
-        if (object.getUserName().equals(object.getUserPlacedLastBet())) {
-            validationResult.add(Error.of("double-bet", "You can't place two bets in a row"));
-            return validationResult;
-        }
-
-        if (userBet <= object.getStartBet() || userBet <=object.getLastBet()) {
-            validationResult.add(Error.of("low-bet", "Your bet should be greater than current bet"));
-            return validationResult;
+            throw new ValidationException(validationResult.getErrors());
         }
 
         if (lotIdsAndBiddersIds.containsKey(lotId)) {
             var biddersId = lotIdsAndBiddersIds.get(lotId);
 
-            if (Objects.equals(object.getUserId(), biddersId.get(biddersId.size() - 1))) {
+            if (Objects.equals(userId, biddersId.get(biddersId.size() - 1))) {
                 validationResult.add(Error.of("double-bet", "You can't place two bets in a row"));
-            return validationResult;
+                throw new ValidationException(validationResult.getErrors());
 
             } else {
-                biddersId.add(object.getUserId());
+                biddersId.add(userId);
             }
         } else {
             lotIdsAndBiddersIds.put(lotId, new ArrayList<>());
             var biddersId = lotIdsAndBiddersIds.get(lotId);
-            biddersId.add(object.getUserId());
+            biddersId.add(userId);
         }
 
-        return validationResult;
+    }
+
+    private void checkInputCharacterInBetField() {
+        var strUserBet = placeBetDto.getUserBet();
+        Integer userBet = null;
+
+        if (!strUserBet.matches("[1-9]+")) {
+            validationResult.add(Error.of("double-bet", "You can use only integer numbers"));
+            throw new ValidationException(validationResult.getErrors());
+        } else {
+            userBet = Integer.valueOf(strUserBet);
+        }
+
+        if (userBet <= placeBetDto.getStartBet() || userBet <=placeBetDto.getLastBet()) {
+            validationResult.add(Error.of("low-bet", "Your bet should be greater than current bet"));
+            throw new ValidationException(validationResult.getErrors());
+        }
     }
 
     public static PlaceBetValidator getInstance() {
